739eb4fb4648379fa00273a5724bf3f5b012f0a9
adnannagdiwala <adnan.nagdiwala@gmail.com>
2026-05-05 02:34:16 +0530

    chore: update project config and dependencies for ATS analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {},
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
